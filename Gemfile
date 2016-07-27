901474bc2618d06e138eff241eb262821391d900
Tinco Andringa <mail@tinco.nl>
2016-07-27 21:05:45 +0200

    Implement lifecycle events for active users

--- Gemfile
+++ Gemfile
@@ -6,6 +6,7 @@ gem 'rails', '~> 5.0.0'
 # Use sqlite3 as the database for Active Record
 gem 'sqlite3'
 gem 'nobrainer'
+gem 'nobrainer_streams', '~> 0.1'
 # Use Puma as the app server
 gem 'puma', '~> 3.0'
 # Use SCSS for stylesheets
--- app/channels/active_users_channel.rb
+++ app/channels/active_users_channel.rb
@@ -1,10 +1,13 @@
 # Be sure to restart your server when you modify this file. Action Cable runs in a loop that does not support auto reloading.
 class ActiveUsersChannel < ApplicationCable::Channel
+  include NoBrainer::Streams
+
   def subscribed
-    # stream_from "some_channel"
+    @user = User.create
+    stream_from User.all, include_initial: true
   end
 
   def unsubscribed
-    # Any cleanup needed when channel is unsubscribed
+    @user.destroy
   end
 end
